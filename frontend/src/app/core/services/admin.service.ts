335a9308f5108ab205d56dc4e7aef4b0828982dc
Lupse <lupse.ioan.victor@gmail.com>
2026-05-15 11:16:07 +0300

    fix : added missing dep

--- frontend/src/app/core/services/admin.service.ts
+++ frontend/src/app/core/services/admin.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { AdminStats, InventoryReport, ImportResult, ProductCreateRequest, SupportTicket } from '../models/admin.model';
+import { AdminStats, InventoryReport, ImportResult, ProductCreateRequest, ProductStock, SupportTicket } from '../models/admin.model';
 import { Order } from '../models/order.model';
 import { Product } from '../models/product.model';
 import { environment } from '../../../environments/environment';
